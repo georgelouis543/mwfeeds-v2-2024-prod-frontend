7c2bbc7655fbf048a8a312994755830b0fa533f3
georgelouis <george.louis@meltwater.com>
2025-01-28 00:09:09 +0530

    adding few more folders and files

---
+++ src/api/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+// const BASE_URL = 'https://backend.mwfeedsv2-staging.com/'
+const BASE_URL = "http://localhost:8000/"
+
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({ 
+    baseURL: BASE_URL,
+    headers: {'Content-Type': 'application/json'},
+    withCredentials: true
+});
